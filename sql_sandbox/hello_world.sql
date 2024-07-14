569b01dc982d2e2556493f34c083ef6f8db0acc1
WolfEYc <wolfeycode@gmail.com>
2024-07-14 18:40:17 -0500

    update thermostatter to use pg from conn str

--- sql_sandbox/hello_world.sql
+++ sql_sandbox/hello_world.sql
@@ -1,17 +1,1 @@
-DROP TABLE hello_world;
-
-CREATE TABLE hello_world (
-    user_name VARCHAR(22),
-    first_name VARCHAR(22),
-    last_name VARCHAR(22)
-);
-
-INSERT INTO
-    hello_world (
-        user_name,
-        first_name,
-        last_name
-    )
-VALUES ('wolfey', 'isaac', 'wolf');
-
-SELECT user_name FROM hello_world;
+-- Active: 1720577098991@@postgres.wolfeydev.com@5432@postgres
